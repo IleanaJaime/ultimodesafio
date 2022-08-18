d883e42bc83bba2ec16cedc841bf56a1ef5cf0eb
Ileana Jaime <ileanamarieljaime@gmail.com>
2022-08-18 08:26:09 -0300

    ultimodesafio

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
 // API SIMPLE
 
-// const divHarry = document.getElementById("divHarry")
+const divHarry = document.getElementById("divHarry")
 const divClima = document.getElementById("divClima")
+const formClima = document.getElementById("formClima")
 const API_KEY = "f9e65ee57960b9731044c2f3d2129615";
-// function consultarPersonaje(){fetch("http://hp-api.herokuapp.com/api/characters/students")
-// .then(response => response.json())
-// .then(({name, species, house, actor}) => {
+
+function consultarPersonaje(){fetch("http://hp-api.herokuapp.com/api/characters/students")
+.then(response => response.json())
+.then(({name, species, house, actor}) => {
     
-//     divHarry.innerHTML = `  
-//     <div>
-//         <h2> Harry Potter Fan</h2>
-//         <p>Nombre:  ${name}</p>
-//         <p>Especies:  ${species}</p>
-//         <p>Casa: ${house}</p>
-//         <p>Actor:  ${actor}</p>
-//     </div>`
-// })
-// }
-// consultarPersonaje()
+    divHarry.innerHTML = `  
+    <div>
+        <h2> Harry Potter Fan</h2>
+        <p>Nombre:  ${name}</p>
+        <p>Especies:  ${species}</p>
+        <p>Casa: ${house}</p>
+        <p>Actor:  ${actor}</p>
+    </div>`
+})
+}
+consultarPersonaje()
 
-// setInterval(() => {
-//     consultarPersonaje()
-// },30000)
+setInterval(() => {
+    consultarPersonaje()
+},30000)
 
 
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q="BuenosAires","LaPlata","Argentina"&appid=${API_KEY}`)
+formClima.addEventListener(`submit`, (e) =>{
+    e.preventDefault()
+    const datForm = new FormData(e.target)
+
+})
+
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${datForm.get("ciudad")},${datForm.get("provincia")},${datForm.get("pais")}&appid=${API_KEY}`)
         .then(response => response.json ())
         .then(data => {
             let { lat, lon, name, country } = data
@@ -33,7 +41,7 @@ const API_KEY = "f9e65ee57960b9731044c2f3d2129615";
             .then(response => response.json())
             .then(({main, weather}) => {
                 let {feels_like :sensacionTermica, humidity: humedad, pressure: presion, temp} = main
-                let desClima = weather[0].
+                let desClima = weather[0].description
                 console.log(sensacionTermica, humedad, presion, temp)
                 console.log(desClima)
                 divClima.innerHTML = ` <div>
